a47f581231da8d52dea48e402a4518b8b8e4d61d
liuwei <liuwei9502@163.com>
2017-11-07 15:33:02 +0800

    新增界面 和跳转功能

--- pages/health/health.js
+++ pages/health/health.js
@@ -6,46 +6,59 @@ Page({
    */
   data: {
     you:false,
-    array: [{
-        title:"title1",
-        _type:1,
-        img:"http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
-        message: 'foo',
-    }, {
-        title: "title2",
-        _type: 2,
-        img: "http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
-         message: 'bar'
-    }]
+    array: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var _this = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        _this.setData({
+          scroll_view: {
+            Height: res.windowHeight,
+            width: res.windowWidth
+          }
+        })
+      },
+    });
+
     var arr = new Array();
-    for(var i = 0; i < 10; i++){
-      if(i%2 == 0){
+    for (var i = 0; i < 10; i++) {
+      if (i % 2 == 0) {
         arr[i] = {
+          id: i,
           _type: 2,
-          title: "title_"+i,
+          title: "title_" + i,
           img: "http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
-          message: 'message_'+i,
+          outline: 'outline_5312354132465jbns,dfgjvzlnklkhkzsfnkuj' + i,
         }
-      }else{
+      } else {
         arr[i] = {
+          id: i,
           _type: 1,
           title: "title_" + i,
           img: "http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
-          message: 'message_' + i,
+          outline: 'outlinegnfuykjrdtgserghsedthnert6hesthetrhdthery_' + i,
         }
       }
     }
     var _this = this;
     _this.setData({
-      array : arr,
+      array: arr,
     })
   },
+
+  click: function (event) {
+    console.log(event)
+    //  <navigator url="/pages/test/test" hover-class="navigator-hover" open-type='navigate'>
+    wx.navigateTo({
+      url: '/pages/test/test?title=' + event.currentTarget.dataset.title,
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
--- pages/my/my.js
+++ pages/my/my.js
@@ -1,3 +1,4 @@
+const app = getApp()
 // pages/my/my.js
 Page({
 
@@ -5,14 +6,82 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    Height: 500,
+    width: 500,
+    array: [{
+      id:1,
+      title: "title1",
+      _type: 1,
+      img: "http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
+      outline: 'foo',
+    }, {
+      id:2,
+      title: "title2",
+      _type: 2,
+      img: "http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
+      outline: 'bar'
+    }]
   },
+
+
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var _this = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        _this.setData({
+          scroll_view:{
+            Height:res.windowHeight,
+            width:res.windowWidth
+          }
+        })
+      },
+    });
+
+    var arr = new Array();
+    for (var i = 0; i < 10; i++) {
+      if (i % 2 == 0) {
+        arr[i] = {
+          id:i,
+          _type: 2,
+          title: "title_" + i,
+          img: "http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
+          outline: 'outline_5312354132465jbns,dfgjvzlnklkhkzsfnkuj' + i,
+        }
+      } else {
+        arr[i] = {
+          id:i,
+          _type: 1,
+          title: "title_" + i,
+          img: "http://7xloj2.com1.z0.glb.clouddn.com/1482733487700",
+          outline: 'outlinegnfuykjrdtgserghsedthnert6hesthetrhdthery_' + i,
+        }
+      }
+    }
+    var _this = this;
+    _this.setData({
+      array: arr,
+    })
+  },
+
+
+click: function(event) {
+    console.log(event)
+    //  <navigator url="/pages/test/test" hover-class="navigator-hover" open-type='navigate'>
+    wx.navigateTo({
+      url: '/pages/test/test?title=' + event.currentTarget.dataset.title,
+    })
   },
 
   /**
