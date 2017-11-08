e1494f54eef4c195d8055cdb921493e6e571ba43
liuwei <liuwei9502@163.com>
2017-11-08 14:33:38 +0800

    备份health

--- pages/health - 副本/health.js
+++ pages/health - 副本/health.js
@@ -6,51 +6,25 @@ Page({
    */
   data: {
     you:false,
-    array: [],
-    swiper_tab_list: [],
-     /** 
-        * 页面配置 
-        */  
-    winWidth: 0,
-    winHeight: 0,
-    // tab切换  
-    currentTab: 0,  
+    array: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-
-    /** 
-     * 获取系统信息 
-     */
+    var _this = this;
     wx.getSystemInfo({
-
       success: function (res) {
-        that.setData({
-          winWidth: res.windowWidth,
-          winHeight: res.windowHeight,
-
-           scroll_view: {
+        _this.setData({
+          scroll_view: {
             Height: res.windowHeight,
             width: res.windowWidth
           }
+        })
+      },
+    });
 
-        });
-      }
-
-    });  
-
-    var swiper_tab_list_arr = new Array();
-    for (var i = 0; i < 4; i++) {
-      swiper_tab_list_arr[i] = {
-        data_current: i,
-        swiper_tab_title: "title_" + i,
-      }
-    }
-    
     var arr = new Array();
     for (var i = 0; i < 10; i++) {
       if (i % 2 == 0) {
@@ -71,30 +45,10 @@ Page({
         }
       }
     }
-    that.setData({
+    var _this = this;
+    _this.setData({
       array: arr,
     })
-
- 
-    that.setData({
-      swiper_tab_list: swiper_tab_list_arr
-    })
-
-
-    // http://www.zhaoyaoba.com/health/cli/notice/getAppStartAd/
-// http://www.zhaoyaoba.com/
-    const requestTask = wx.request({
-      url: "https://www.zhaoyaoba.com/health/cli/notice/getAppStartAd/",
-      header: {
-        'content-type': 'application/json'
-      },
-      success: function (res) {
-        console.log(res)
-      }
-    })
-
-    // requestTask.abort();// 取消请求任务
-
   },
 
   click: function (event) {
@@ -105,30 +59,6 @@ Page({
     })
   },
 
-  /** 
-      * 滑动切换tab 
-      */
-  bindChange: function (e) {
-
-    var that = this;
-    that.setData({ currentTab: e.detail.current });
-
-  },
-  /** 
-   * 点击tab切换 
-   */
-  swichNav: function (e) {
-
-    var that = this;
-
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
-    }
-  }, 
 
   /**
    * 生命周期函数--监听页面初次渲染完成
